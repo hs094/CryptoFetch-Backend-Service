2ededbae6fa3480315ae75ab07a3ac4c9c3b7bd6
iamhardikat11 <iamhardikat11@gmail.com>
2025-01-11 12:01:19 +0530

    Made the Job Service and Creted 2 End-Points

---
+++ src/crypto-data/controller/stats.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CryptoDataService } from '../crypto-data.service';
+import { Logger } from '@nestjs/common';
+
+@Controller('stats')
+export class StatsController {
+  constructor(private readonly cryptoDataService: CryptoDataService) {}
+  private readonly logger = new Logger(StatsController.name);
+
+  @Post()
+  async getStats(@Body('coin') coin: string) {
+    if (!coin) {
+      this.logger.error('Coin parameter is missing, defaulting to "bitcoin"');
+      coin = 'bitcoin'; // Default to 'bitcoin'
+    }
+    return await this.cryptoDataService.fetchLatestStats(coin);
+  }
+}
